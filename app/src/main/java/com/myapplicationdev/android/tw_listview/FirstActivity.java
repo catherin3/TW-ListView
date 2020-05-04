fea9fc5755672ae059dc8628a7a091b74c678ef3
catherine <18016240@myrp.edu.sg>
2020-05-04 12:30:04 +0800

    DMSD

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -18,9 +18,20 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = (ListView) this.findViewById(R.id.lvYear);
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+
+        aa= new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,al);
+        lv.setAdapter(aa);
+
+
+
 
 
 
--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,8 +1,13 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ModuleAdapter {
+
 
     private Context context;
 
